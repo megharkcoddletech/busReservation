a5b5bb256f18a586a72f4e95a642904cebd6a175
Megha R K <meghasuryark@gmail.com>
2023-11-02 10:18:49 +0530

    db connection

---
+++ db_connection.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql2');
+var con = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password : "S006ya#1",
+    database: "megha",
+    port : 3000,
+});
+con.connect(function(err){
+    if(err) { 
+    console.log("connected");
+    }
+})
