16924643c88a849189ec3f7e1ea744ccbdfe246b
Megha R K <meghasuryark@gmail.com>
2023-11-09 12:03:51 +0530

    get buses by time

--- controllers/busController.js
+++ controllers/busController.js
@@ -2,12 +2,16 @@
 const busModel = require('../models/busModels');
 
 const getBusController = async(req, res) => {
-    const { starting_point, destination } = req.body;
-    const busController = await busModel.viewBus(starting_point, destination, (err, result) => {
+    const { starting_point, destination, boarding_time} = req.body;
+    const body = req.body;
+    const busController = await busModel.viewBus(starting_point, destination, boarding_time,(err, result) => {
         if (err) {
             res.status(500).json({ success: false, message: "internal server error" });
         } else if (result.length > 0) {
-            if(starting_point=== null && destination=== null){ 
+            if(body.starting_point=== null && body.destination=== null && body.boarding_time === null){ 
+                res.status(200).json({success: "true", result})
+            }
+            else if(body.boarding_time === null){
                 res.status(200).json({success: "true", result})
             }
             else{
@@ -17,7 +21,12 @@ const getBusController = async(req, res) => {
              else {
             res.status(400).json({ success: false });
         }
+
     });
+    let result =await Promise.all([
+        busController
+    ])
+    return result
 };
 
 module.exports = {
--- models/busModels.js
+++ models/busModels.js
@@ -1,16 +1,21 @@
 const busdb = require('../db_connection');
 
-const viewBus = async(starting_point, destination)=>{
+const viewBus = async(starting_point, destination, boarding_time)=>{
     const getAllBus = `select * from bus`;
     const getBusByRoute = `select bus.name, route.starting_point, route.destination, 
     route.boarding_time, route.deboarding_time FROM bus INNER JOIN route
      ON bus.id = route.bus_id WHERE route.starting_point = ? AND route.destination = ?`;
    
-
+   const getBusByTime = `select bus.name, route.starting_point, route.destination, 
+   route.boarding_time, route.deboarding_time FROM bus INNER JOIN route
+    ON bus.id = route.bus_id WHERE route.starting_point =? AND route.destination = ?  and route.boarding_time = ?;`
  let results = await Promise.all(([
     busdb.con.promise().query(getAllBus),
-    busdb.con.promise().query(getBusByRoute, [starting_point, destination])
+    busdb.con.promise().query(getBusByRoute, [starting_point, destination]),
+    busdb.con.promise().query(getBusByTime, [starting_point, destination, boarding_time])
  ]))
+
+ return results;
 
 }
 
