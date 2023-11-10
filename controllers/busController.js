b70be21e3452691cbfe8c7de00d4448f1224dc96
Megha R K <meghasuryark@gmail.com>
2023-11-10 09:27:14 +0530

    fetching buses

--- controllers/busController.js
+++ controllers/busController.js
@@ -1,34 +1,21 @@
-
 const busModel = require('../models/busModels');
-
-const getBusController = async(req, res) => {
+const getBus = async(req, res) => {   
     const { starting_point, destination, boarding_time} = req.body;
     const body = req.body;
-    const busController = await busModel.viewBus(starting_point, destination, boarding_time,(err, result) => {
-        if (err) {
-            res.status(500).json({ success: false, message: "internal server error" });
-        } else if (result.length > 0) {
-            if(body.starting_point=== null && body.destination=== null && body.boarding_time === null){ 
-                res.status(200).json({success: "true", result})
-            }
-            else if(body.boarding_time === null){
-                res.status(200).json({success: "true", result})
-            }
-            else{
-                res.status(200).json({success: "true", result})
-            }
-        }
-             else {
-            res.status(400).json({ success: false });
-        }
+    const buses = await busModel.viewBus(starting_point, destination, boarding_time);
+    if (buses.length > 0) {
+      if (body.starting_point != null && body.destination != null && body.boarding_time != null) {
+          return res.status(200).json({ success: "true", result: buses[0][0] });
+      } else if (body.starting_point != null && body.destination != null && body.boarding_time === null) {
+          return res.status(200).json({ success: "true", result: buses[0][0] });
+      } else {
+          return res.status(200).json({ success: "true", result: buses[0][0] });
+      }
+  } else {
+      return res.status(400).json({ success: "false", message: "error occurred" });
+  }
 
-    });
-    let result =await Promise.all([
-        busController
-    ])
-    return result
 };
-
 module.exports = {
-    getBusController
+    getBus
 }
--- models/busModels.js
+++ models/busModels.js
@@ -1,24 +1,36 @@
 const busdb = require('../db_connection');
+const viewBus = async (starting_point, destination, boarding_time) => {
+    try {
+        let allArrry = [];
+        const getAllBus = `select * from bus`;
+        const getBusByRoute = `select bus.name, route.starting_point, route.destination, 
+        route.deboarding_time FROM bus INNER JOIN route
+        ON bus.id = route.bus_id WHERE route.starting_point = ? AND route.destination = ?`;
 
-const viewBus = async(starting_point, destination, boarding_time)=>{
-    const getAllBus = `select * from bus`;
-    const getBusByRoute = `select bus.name, route.starting_point, route.destination, 
-    route.boarding_time, route.deboarding_time FROM bus INNER JOIN route
-     ON bus.id = route.bus_id WHERE route.starting_point = ? AND route.destination = ?`;
-   
-   const getBusByTime = `select bus.name, route.starting_point, route.destination, 
-   route.boarding_time, route.deboarding_time FROM bus INNER JOIN route
-    ON bus.id = route.bus_id WHERE route.starting_point =? AND route.destination = ?  and route.boarding_time = ?;`
- let results = await Promise.all(([
-    busdb.con.promise().query(getAllBus),
-    busdb.con.promise().query(getBusByRoute, [starting_point, destination]),
-    busdb.con.promise().query(getBusByTime, [starting_point, destination, boarding_time])
- ]))
+        const getBusByTime = `select bus.name, route.starting_point, route.destination, 
+        route.boarding_time, route.deboarding_time FROM bus INNER JOIN route
+        ON bus.id = route.bus_id WHERE route.starting_point =? AND route.destination = ?  and route.boarding_time = ?`;
 
- return results;
+        const allBus = await busdb.con.promise().query(getAllBus);
+        const allROute = await busdb.con.promise().query(getBusByRoute, [starting_point, destination]);
+        const allTime = await busdb.con.promise().query(getBusByTime, [starting_point, destination, boarding_time]);
+        if(starting_point!= null && destination != null && boarding_time != null){ 
+            allArrry.push(allTime);
+        }
+        else if(starting_point!= null && destination != null && boarding_time == null){
+          allArrry.push(allROute);
+        }
+        else {
+           allArrry.push(allBus)
+        }
+
+        return allArrry;
+    } catch (error) {
+        console.log(error);
+    }
 
 }
-
-module.exports ={
+module.exports = {
     viewBus
 }
+
