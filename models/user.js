b921f37d641588346a4cd816f4954e68f089ca56
Megha R K <meghasuryark@gmail.com>
2023-11-15 09:30:50 +0530

    user authentication

--- models/user.js
+++ models/user.js
@@ -1,31 +1,37 @@
 const userdb = require('../db_connection');
-const db = userdb.makeDb(userdb)
+const db = userdb.makeDb(userdb);
 const createUser = async (name, username, password, email, age, contact_number, gender) => {
 
-    try{
-        const qr = `insert into customer (name, username, password, email, age, contact_number, gender) values(?, ?, ?, ?, ?, ?, ?)`;
-        const addUser = await db.query(qr, [name, username, password, email, age, contact_number, gender]);
-        return addUser;
+    try {
+        const users = `select * from customer where email = ?`;
+        const checkUser = await db.query(users, [email]);
+        if (checkUser.length > 0) {
+            return checkUser;
+        }
+        else {
+            const qr = `insert into customer (name, username, password, email, age, contact_number, gender) values(?, ?, ?, ?, ?, ?, ?)`;
+            const addUser = await db.query(qr, [name, username, password, email, age, contact_number, gender]);
+            return addUser;
+        }     
+
     }
-    catch(err){
+    catch (err) {
         await db.close();
     }
 }
 
 const login = async (username, password) => {
 
-   try {
-     const loginQr = `select username, password from customer where username=? and password=?`;
-     const userLogin = await db.query(loginQr, [username, password]);
-     return userLogin;
-   }
-   catch(err){
-    await db.close();
-   }
+    try {
+        const loginQr = `select username, password from customer where username=? and password=?`;
+        const userLogin = await db.query(loginQr, [username, password]);
+        return userLogin;
+    }
+    catch (err) {
+        await db.close();
+    }
 }
 
 module.exports = {
     createUser, login
 }
-
-
