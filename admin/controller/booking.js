03669c25188a83f3e22632dd67a35a889b85c59a
Megha R K <meghasuryark@gmail.com>
2023-12-11 15:32:50 +0530

    corrected

--- admin/controller/booking.js
+++ admin/controller/booking.js
@@ -35,7 +35,7 @@ const viewTickets = async (req, res) => {
           success: true, data: tickets, page: `${page}}`,
         });
       } else {
-        res.status(200).json({ success: true, message: 'no tickets' });
+        res.status(200).json({ success: false, message: 'no tickets' });
       }
     }
   } catch (err) {
--- user/controller/booking.js
+++ user/controller/booking.js
@@ -71,7 +71,7 @@ const viewTickets = async (req, res) => {
           success: true, data: tickets, page: `${page}`,
         });
       } else {
-        res.status(200).json({ success: true, message: 'no tickets' });
+        res.status(200).json({ success: false, message: 'no tickets' });
       }
     }
   } catch (err) {
